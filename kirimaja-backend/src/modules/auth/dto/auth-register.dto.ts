16736714ba831f163727e97dfdb35d0319ee53ce
laelachoi <laelachoirotunnisa22@gmail.com>
2025-08-24 22:15:32 +0700

    Update
    - BE auth login
    - BE auth register

---
+++ kirimaja-backend/src/modules/auth/dto/auth-register.dto.ts
@@ -0,0 +1,39 @@
+import { z, ZodObject } from "zod"
+
+const authRegisterSchema = z.object({
+    name: z
+        .string({
+            required_error: 'Name is required',
+            invalid_type_error: 'Name must be a string',
+        })
+        .min(1, "Name is required"),
+    email: z
+        .string({
+            required_error: 'Email is required',
+            invalid_type_error: 'Email must be a string',
+        }).email({
+            message: 'Invalid email format',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+            invalid_type_error: 'Password must be a string',
+        })
+        .min(8, "Password must be at least 8 characters long"),
+    phone_number: z
+        .string({
+            required_error: 'Phone Number is required',
+            invalid_type_error: 'Phone Number must be a string',
+        })
+        .min(10, "Phone Number must be at least 10 characters long"),
+});
+
+export class AuthRegisterDto {
+    static schema: ZodObject<any> = authRegisterSchema
+    constructor(
+        public readonly name: string,
+        public readonly email: string,
+        public readonly password: string,
+        public readonly phone_number: string,
+    ) {}
+}
